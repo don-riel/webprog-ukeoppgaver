67b3ceb22a07d2cd621093893a48c8dc3154a8ad
Donni <donni.garvida@gmail.com>
2021-04-08 18:34:17 +0200

    opprettet quiz.js fil

--- src/main/resources/static/quiz.js
+++ src/main/resources/static/quiz.js
@@ -56,7 +56,7 @@ function createOppggave(choices) {
             altList.appendChild(document.createElement("br"))
         }
         quest.appendChild(altList)
-        
+
     }
     const btn = document.createElement("button");
     btn.innerText = "Sjekk svar";
@@ -76,45 +76,3 @@ function createOppggave(choices) {
 
 createOppggave(oppgaver);
 
-// ========================== //
-function leggTil() {
-    const input = document.getElementById("inputTodo");
-    const div = document.getElementsByClassName("todoList")[0];
-
-    if(input.value.length) {
-        addTodo(input.value, div, "test");
-    }
-
-}
-
-function addTodo(value, parent, id) {
-    const box = document.createElement('div');
-    const child = document.createElement('input');
-    const label = document.createElement('label');
-    const doneList = document.getElementsByClassName("done")[0];
-
-    child.type = 'checkbox';
-    child.id = id;
-    child.value = value;
-    child.innerHTML = value;
-    child.addEventListener('click', function() {
-
-        if(this.checked) {
-            this.parentElement.style.textDecoration = 'line-through';
-            doneList.appendChild(this.parentElement.parentElement)
-
-        } else {
-            this.parentElement.style.textDecoration = 'none';
-        }
-    })
-
-    label.htmlFor = id;
-    label.innerHTML = value;
-
-
-    label.insertAdjacentElement('beforeend', child);
-    box.appendChild(label);
-    parent.insertAdjacentElement("beforeend", box);
-
-
-}
---
+++ src/main/resources/static/todo.js
@@ -0,0 +1,37 @@
+// ========================== //
+function leggTil() {
+    const input = document.getElementById("inputTodo");
+    const div = document.getElementsByClassName("todoList")[0];
+
+    if(input.value.length) {
+        addTodo(input.value, div, "test");
+    }
+}
+
+function addTodo(value, parent, id) {
+    const box = document.createElement('div');
+    const child = document.createElement('input');
+    const label = document.createElement('label');
+    const doneList = document.getElementsByClassName("done")[0];
+
+    child.type = 'checkbox';
+    child.id = id;
+    child.value = value;
+    child.innerHTML = value;
+    child.addEventListener('click', function() {
+
+        if(this.checked) {
+            this.parentElement.style.textDecoration = 'line-through';
+            doneList.appendChild(this.parentElement.parentElement)
+
+        } else {
+            this.parentElement.style.textDecoration = 'none';
+        }
+    })
+
+    label.htmlFor = id;
+    label.innerHTML = value;
+    label.insertAdjacentElement('beforeend', child);
+    box.appendChild(label);
+    parent.insertAdjacentElement("beforeend", box);
+}
